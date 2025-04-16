f9bb14d95833c8dc6092476c225f943fb2be66ef
khaldoun36 <khaldoun36@gmail.com>
2025-04-16 10:38:29 +0400

    created xcode project

---
+++ Step Tracker April/Step_Tracker_AprilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_Tracker_AprilApp.swift
+//  Step Tracker April
+//
+//  Created by khaldoun alnuaimi on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_Tracker_AprilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
